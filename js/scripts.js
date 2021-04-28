54868fb10a0c8fc8737fb281351dc5858f14917a
2tefan <2tefan@protonmail.com>
2021-04-28 12:43:11 +0200

    Added regulating distance

--- js/scripts.js
+++ js/scripts.js
@@ -3,16 +3,24 @@ google.charts.setOnLoadCallback(drawChart);
 
 var arr = [['t', 'u1']];
 
-for (i = 0; i < 20; i++) {
-    arr.push([i, Math.random() * 100]);
-}
+// regulating distance
+var T2 = 1550; // s
+var Uc = 0; // V
+var Ue = 1;
+var dt = 1;
 
+var Tmax = T2 * 5; // 5 * τ
+
+for (i = 0; i < Tmax; i++) {
+    Uc = Uc + dt / T2 * (Ue - Uc);
+    arr.push([i, Uc]);
+}
 
 function drawChart() {
     var data = google.visualization.arrayToDataTable(arr);
 
     var options = {
-        title: 'Regler',
+        title: 'LP. 1st order',
         curveType: 'function',
         legend: { position: 'bottom' }
     };
