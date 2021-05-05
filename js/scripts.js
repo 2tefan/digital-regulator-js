efb811a468618b29dc2888c276398b9245a409da
2tefan <2tefan@protonmail.com>
2021-05-05 12:19:03 +0200

    Added sin

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,43 @@
 google.charts.load('current', { 'packages': ['corechart'] });
 google.charts.setOnLoadCallback(drawLowPass);
+google.charts.setOnLoadCallback(drawLowPassSin);
 
+number = 11
+T1 = 100 + 5 * number // s
+T2 = T1 * 10
 
-function lowPass(){
+Vs = 0.8 + number / 100
+targetValue = 10 + number
+
+function lowPassSin(){
+    var lowPass = [];
+    var u = 0; // V
+
+    var Tmax = T2 * 15; // 5 * τ
+
+    for (i = 0; i < Tmax; i++) {
+        u = 10 * Math.sin(2*3.14*102.68e-6*i);
+        lowPass.push([i, u, null, null]);
+    }
+
+    return lowPass
+}
+
+function lowPass() {
     var lowPass = [];
 
     // regulating distance
-    var T2 = 1550; // s
     var Uc = 0; // V
     var Ue = 1;
     var dt = 1;
-    
+
     var Tmax = T2 * 5; // 5 * τ
-    
+
     for (i = 0; i < Tmax; i++) {
         Uc = Uc + dt / T2 * (Ue - Uc);
         lowPass.push([i, Uc, i == T2 ? Uc : null, null]);
     }
-    
+
     drawPoint("τ", lowPass, T2)
     return lowPass
 }
@@ -26,7 +46,7 @@ function lowPass(){
 function drawLowPass() {
     var data = new google.visualization.DataTable();
     data.addColumn('number', 't');
-    data.addColumn('number', 'Uout');
+    data.addColumn('number', 'u');
     data.addColumn('number', 'markedPoints');
     data.addColumn({ type: 'string', role: 'annotation' });
 
@@ -54,7 +74,39 @@ function drawLowPass() {
     chart.draw(data, options);
 }
 
-function drawPoint(name, arr, T){
+
+function drawLowPassSin() {
+    var data = new google.visualization.DataTable();
+    data.addColumn('number', 't');
+    data.addColumn('number', 'Uout');
+    data.addColumn('number', 'markedPoints');
+    data.addColumn({ type: 'string', role: 'annotation' });
+
+    data.addRows(lowPassSin())
+
+    var options = {
+        title: 'Low-pass filter 1st order',
+        curveType: 'function',
+        legend: { position: 'bottom' },
+        hAxis: { title: 'Time [ms]', gridlines: { interval: [1,2,5], count: 8 }, minorGridlines: { interval: 0.5 }, },
+        vAxis: { title: 'Voltage [V]', gridlines: { interval: 1 / 8, count: 8 }, },
+        series: {
+            1: {
+                annotations: {
+                    textStyle: { fontSize: 15, color: 'red' },
+                },
+                pointSize: 5,
+                visibleInLegend: false
+            },
+        }
+    };
+
+    var chart = new google.visualization.LineChart(document.getElementById('low_pass_sin'));
+
+    chart.draw(data, options);
+}
+
+function drawPoint(name, arr, T) {
     arr[T][3] = name + " [" + T + "/" + formatFloat(arr[T][1]) + "]";
 }
 
