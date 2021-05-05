0be4bb563529745f12f54f414837d4c9e88e8092
2tefan <2tefan@protonmail.com>
2021-05-05 12:31:47 +0200

    Improved sin

--- js/scripts.js
+++ js/scripts.js
@@ -9,15 +9,18 @@ T2 = T1 * 10
 Vs = 0.8 + number / 100
 targetValue = 10 + number
 
-function lowPassSin(){
+function lowPassSin() {
     var lowPass = [];
-    var u = 0; // V
+    var Usin = 0;
+    var Uc = 0;
+    var dt = 1;
 
-    var Tmax = T2 * 15; // 5 * τ
+    var Tmax = T2 * 25; // 5 * τ
 
     for (i = 0; i < Tmax; i++) {
-        u = 10 * Math.sin(2*3.14*102.68e-6*i);
-        lowPass.push([i, u, null, null]);
+        Usin = 10 * Math.sin(1 / T2 * i);
+        Uc = Uc + dt / T2 * (Usin - Uc);
+        lowPass.push([i, Uc, Usin, null]);
     }
 
     return lowPass
@@ -46,7 +49,7 @@ function lowPass() {
 function drawLowPass() {
     var data = new google.visualization.DataTable();
     data.addColumn('number', 't');
-    data.addColumn('number', 'u');
+    data.addColumn('number', 'Uout');
     data.addColumn('number', 'markedPoints');
     data.addColumn({ type: 'string', role: 'annotation' });
 
@@ -79,24 +82,20 @@ function drawLowPassSin() {
     var data = new google.visualization.DataTable();
     data.addColumn('number', 't');
     data.addColumn('number', 'Uout');
-    data.addColumn('number', 'markedPoints');
+    data.addColumn('number', 'Usin');
     data.addColumn({ type: 'string', role: 'annotation' });
 
     data.addRows(lowPassSin())
 
     var options = {
-        title: 'Low-pass filter 1st order',
+        title: 'Low-pass filter 1st order with sin',
         curveType: 'function',
         legend: { position: 'bottom' },
-        hAxis: { title: 'Time [ms]', gridlines: { interval: [1,2,5], count: 8 }, minorGridlines: { interval: 0.5 }, },
-        vAxis: { title: 'Voltage [V]', gridlines: { interval: 1 / 8, count: 8 }, },
+        hAxis: { title: 'Time [ms]', gridlines: { interval: [1, 2, 5], count: 8 }, minorGridlines: { interval: 0.5 }, },
+        vAxis: { title: 'Voltage [V]', gridlines: { interval: [1, 2, 5], count: 8 }, },
         series: {
             1: {
-                annotations: {
-                    textStyle: { fontSize: 15, color: 'red' },
-                },
-                pointSize: 5,
-                visibleInLegend: false
+                type: 'line', lineDashStyle: [2, 2]
             },
         }
     };
