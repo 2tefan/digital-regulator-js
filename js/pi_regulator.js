13f949d84c60a09a7d4433bb6d77afe76002db09
2tefan <2tefan@protonmail.com>
2021-05-26 12:29:29 +0200

    Add overshoot

--- js/pi_regulator.js
+++ js/pi_regulator.js
@@ -1,6 +1,7 @@
 google.charts.setOnLoadCallback(drawPRegulator);
+google.charts.setOnLoadCallback(drawPIRegulator);
 
-function getKp(){
+function getKp() {
     return T2 / T1 * Math.sqrt(2) / (2 * Vs);
 }
 
@@ -12,6 +13,7 @@ function pRegulator() {
     let dt = 1;
     let Tmax = T2 * 3;
     let Ua = 0;
+    let Uamax = 0;
 
     let Kp = getKp();
 
@@ -36,7 +38,8 @@ function pRegulator() {
         if (!maxValuePassed && Ua_n1 > Ua) {
             maxValuePassed = true;
             arr[i - 1][2] = Ua;
-            drawPoint("Umax", arr, i - 1, 3)
+            drawPoint("Umax", arr, i - 1, 3);
+            Uamax = Ua;
         }
 
         if (maxValuePassed && !minValuePassed && Ua_n1 < Ua) {
@@ -52,6 +55,67 @@ function pRegulator() {
     arr[Tmax - 1][6] = Ua;
     drawPoint("Udev", arr, i - 1, 7)
 
+    $("#p_regulator_overshoot").text(formatFloat((Uamax / Ua - 1) * 100) + " %");
+    return arr;
+}
+
+function piRegulator() {
+    let arr = [];
+    let Up = 0;
+    let Ui = 0;
+    let Ur = 0;
+
+    let Uc1 = 0;
+    let Uc2 = 0;
+    let dt = 1;
+    let Tmax = T2 * 3;
+    let Ua = 0;
+
+    let Kp = getKp();
+    let Tn = T2;
+    let Ti = Tn / Kp;
+
+    let Ua_n1 = Ua
+
+    let maxValuePassed = false
+    let minValuePassed = false
+    let e = 0;
+
+    
+    for (i = 0; i < Tmax; i++) {
+        e = targetValue - Ua;
+        Up = Kp * e;
+        Ui = Ui + e * dt/Ti;
+        Ur = Up + Ui;
+
+        Uc1 = Uc1 + dt / T1 * (Ur - Uc1);
+        Uc2 = Uc2 + dt / T2 * (Uc1 - Uc2);
+        Ua = Uc2 * Vs
+
+        arr.push([i, Ua, null, null, null, null, null, null, targetValue]);
+
+
+        if (!maxValuePassed && Ua_n1 > Ua) {
+            maxValuePassed = true;
+            arr[i - 1][2] = Ua;
+            drawPoint("Umax", arr, i - 1, 3);
+            Uamax = Ua;
+        }
+
+        if (maxValuePassed && !minValuePassed && Ua_n1 < Ua) {
+            minValuePassed = true;
+            arr[i - 1][4] = Ua;
+            drawPoint("Umin", arr, i - 1, 5)
+        }
+
+        Ua_n1 = Ua
+    }
+
+    // control deviation
+    arr[Tmax - 1][6] = Ua;
+    drawPoint("Udev", arr, i - 1, 7)
+
+    $("#pi_regulator_overshoot").text(formatFloat((Uamax / Ua - 1) * 100) + " %");
     return arr;
 }
 
@@ -90,6 +154,46 @@ function drawPRegulator() {
     }
 
     let chart = new google.visualization.LineChart(document.getElementById('p_regulator'));
+
+    chart.draw(data, options);
+}
+
+
+function drawPIRegulator() {
+    let data = new google.visualization.DataTable();
+    data.addColumn('number', 't');
+    data.addColumn('number', 'Uout');
+    data.addColumn('number', 'Max');
+    data.addColumn({ type: 'string', role: 'annotation' });
+    data.addColumn('number', 'Min');
+    data.addColumn({ type: 'string', role: 'annotation' });
+    data.addColumn('number', 'control deviation');
+    data.addColumn({ type: 'string', role: 'annotation' });
+    data.addColumn('number', 'Target value');
+
+    data.addRows(piRegulator());
+
+    let options = getDefaultOptions('PI-Regulator [ Kp = ' + formatFloat(getKp()) + ' ]');
+    options.series = {
+        1: {
+            pointSize: 5,
+            visibleInLegend: false
+        },
+        2: {
+            pointSize: 5,
+            visibleInLegend: false
+        },
+        3: {
+            pointSize: 5,
+            visibleInLegend: false
+        },
+    }
+
+    options.annotations = {
+        fontSize: 15
+    }
+
+    let chart = new google.visualization.LineChart(document.getElementById('pi_regulator'));
 
     chart.draw(data, options);
 }
