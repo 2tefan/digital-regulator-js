252c3145f44d2dd975f1440b0e465239e935974f
2tefan <2tefan@protonmail.com>
2021-06-23 12:42:42 +0200

    Add DAC sin

--- js/pwnDac.js
+++ js/pwnDac.js
@@ -1,5 +1,6 @@
 google.charts.setOnLoadCallback(drawCount);
 google.charts.setOnLoadCallback(drawPWM);
+google.charts.setOnLoadCallback(drawCountSin);
 
 function countOutput() {
   let arr = [];
@@ -31,8 +32,6 @@ function pwmOutput() {
   let Tmax = Tpwm * 20;
   let targetNumber = 50 + 10 * number;
 
-  console.log(Tpwm);
-
   for (i = 0; i < Tmax; i += dt) {
     Ucount = i % Tpwm;
 
@@ -40,6 +39,32 @@ function pwmOutput() {
     else Uout = 0;
 
     arr.push([i, Uout]);
+  }
+
+  return arr;
+}
+
+function countOutputSin() {
+  let arr = [];
+  let dt = 1;
+  let Ucount = 0;
+
+  let Tpwm = 100 + 10 * number;
+  let Tmax = Tpwm * 20;
+
+  let amplitude = Tpwm / 2;
+  let Tsin = 20 * Tpwm;
+
+  for (i = 0; i < Tmax; i += dt) {
+    Ucount++;
+
+    if (i % Tpwm == 0) Ucount = 0;
+
+    arr.push([
+      i,
+      Ucount,
+      amplitude * Math.sin((1 / Tsin) * 2 * Math.PI * i) + amplitude,
+    ]);
   }
 
   return arr;
@@ -73,6 +98,23 @@ function drawPWM() {
 
   let chart = new google.visualization.LineChart(
     document.getElementById("pwm_out")
+  );
+
+  chart.draw(data, options);
+}
+
+function drawCountSin() {
+  let data = new google.visualization.DataTable();
+  data.addColumn("number", "t");
+  data.addColumn("number", "Z");
+  data.addColumn("number", "Zin");
+
+  data.addRows(countOutputSin());
+
+  let options = getDefaultOptionsDAC("Counter");
+
+  let chart = new google.visualization.LineChart(
+    document.getElementById("count_out_sin")
   );
 
   chart.draw(data, options);
